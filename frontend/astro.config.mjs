9ef72e2f0b0c23b0a25233f9b3289c31b0f0f7c0
Thesportydar <ipaladinobravo@gmail.com>
2025-12-06 19:17:13 -0300

    domain change

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
-  site: "https://fintwit.inaqui.me"
+  site: "https://fintwit.com.ar"
 });
