54479992f432ea3a62dadb7acc375c94611616e9
Iñaqui Paladino <ipaladinobravo@gmail.com>
2025-12-31 16:54:53 -0300

    update feriados 2026

--- lambdas/processor/index.mjs
+++ lambdas/processor/index.mjs
@@ -12,39 +12,42 @@ const s3Client = new S3Client();
 
 const feriados = [
     { mes: 1, dia: 1, motivo: "Año nuevo", tipo: "inamovible" },
-    { mes: 3, dia: 3, motivo: "Carnaval", tipo: "inamovible" },
-    { mes: 3, dia: 4, motivo: "Carnaval", tipo: "inamovible" },
+    { mes: 2, dia: 16, motivo: "Carnaval", tipo: "inamovible" },
+    { mes: 2, dia: 17, motivo: "Carnaval", tipo: "inamovible" },
+    { mes: 3, dia: 23, motivo: "Día no laborable con fines turísticos", tipo: "puente" },
     { mes: 3, dia: 24, motivo: "Día Nacional de la Memoria por la Verdad y la Justicia", tipo: "inamovible" },
     { mes: 4, dia: 2, motivo: "Día del Veterano y de los Caídos en la Guerra de Malvinas", tipo: "inamovible" },
-    { mes: 4, dia: 18, motivo: "Viernes Santo", tipo: "inamovible" },
+    { mes: 4, dia: 3, motivo: "Viernes Santo", tipo: "inamovible" },
     { mes: 5, dia: 1, motivo: "Día del Trabajador", tipo: "inamovible" },
-    { mes: 5, dia: 2, motivo: "Puente turístico no laborable", tipo: "puente" },
     { mes: 5, dia: 25, motivo: "Día de la Revolución de Mayo", tipo: "inamovible" },
-    { mes: 6, dia: 16, motivo: "Paso a la Inmortalidad del General Martín Güemes", tipo: "trasladable" },
+    { mes: 6, dia: 15, motivo: "Paso a la Inmortalidad del General Martín Güemes", tipo: "trasladable" },
     { mes: 6, dia: 20, motivo: "Paso a la Inmortalidad del General Manuel Belgrano", tipo: "inamovible" },
     { mes: 7, dia: 9, motivo: "Día de la Independencia", tipo: "inamovible" },
-    { mes: 8, dia: 15, motivo: "Puente turístico no laborable", tipo: "puente" },
+    { mes: 7, dia: 10, motivo: "Puente turístico no laborable", tipo: "puente" },
     { mes: 8, dia: 17, motivo: "Paso a la Inmortalidad del Gral. José de San Martín", tipo: "trasladable" },
-    { mes: 10, dia: 10, motivo: "Puente turístico no laborable", tipo: "puente" },
-    { mes: 10, dia: 12, motivo: "Día del Respeto a la Diversidad Cultural", tipo: "trasladable" },
-    { mes: 11, dia: 21, motivo: "Puente turístico no laborable", tipo: "puente" },
-    { mes: 11, dia: 24, motivo: "Día de la Soberanía Nacional", tipo: "trasladable" },
+    { mes: 10, dia: 12, motivo: "Día de la Raza", tipo: "trasladable" },
+    { mes: 11, dia: 23, motivo: "Día de la Soberanía Nacional", tipo: "trasladable" },
+    { mes: 12, dia: 7, motivo: "Puente turístico no laborable", tipo: "puente" },
     { mes: 12, dia: 8, motivo: "Día de la Inmaculada Concepción de María", tipo: "inamovible" },
-    { mes: 12, dia: 25, motivo: "Navidad", tipo: "inamovible" }
+    { mes: 12, dia: 25, motivo: "Navidad", tipo: "inamovible" },
+    { mes: 12, dia: 31, motivo: "Fin de Año", tipo: "inamovible" }
 ];
 
 // --- CONFIGURACIÓN ---
 const feriadosUSA = [
     { mes: 1, dia: 1, motivo: "New Year's Day", tipo: "Federal" },
-    { mes: 1, dia: 20, motivo: "Martin Luther King Jr. Day", tipo: "Federal (Market Holiday)" },
-    { mes: 2, dia: 17, motivo: "Washington’s Birthday (Presidents’ Day)", tipo: "Federal (Market Holiday)" },
-    { mes: 3, dia: 28, motivo: "Good Friday", tipo: "Religious (Market Holiday)" }, // No es federal, pero sí cierre de mercado
-    { mes: 5, dia: 26, motivo: "Memorial Day", tipo: "Federal (Market Holiday)" },
+    { mes: 1, dia: 19, motivo: "Martin Luther King Jr. Day", tipo: "Federal" },
+    { mes: 2, dia: 16, motivo: "Washington's Birthday (Presidents' Day)", tipo: "Federal" },
+    { mes: 3, dia: 3, motivo: "Good Friday", tipo: "Observance" },
+    { mes: 5, dia: 25, motivo: "Memorial Day", tipo: "Federal" },
     { mes: 6, dia: 19, motivo: "Juneteenth National Independence Day", tipo: "Federal" },
-    { mes: 7, dia: 4, motivo: "Independence Day", tipo: "Federal (Market Holiday)" },
-    { mes: 9, dia: 1, motivo: "Labor Day", tipo: "Federal (Market Holiday)" },
-    { mes: 11, dia: 27, motivo: "Thanksgiving Day", tipo: "Federal (Market Holiday)" },
-    { mes: 12, dia: 25, motivo: "Christmas Day", tipo: "Federal (Market Holiday)" }
+    { mes: 7, dia: 3, motivo: "Independence Day (Observed)", tipo: "Federal" },
+    { mes: 7, dia: 4, motivo: "Independence Day", tipo: "Federal" },
+    { mes: 9, dia: 7, motivo: "Labor Day", tipo: "Federal" },
+    { mes: 10, dia: 12, motivo: "Columbus Day", tipo: "Federal" },
+    { mes: 11, dia: 11, motivo: "Veterans Day", tipo: "Federal" },
+    { mes: 11, dia: 26, motivo: "Thanksgiving Day", tipo: "Federal" },
+    { mes: 12, dia: 25, motivo: "Christmas Day", tipo: "Federal" }
 ];
 
 // --- FUNCIONES AUXILIARES ---
